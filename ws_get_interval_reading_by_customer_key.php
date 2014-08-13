0bc672fb34cac1eb98c2c609de1f0c34fb03fbf2
Herve Senot <herve.senot@groundtruth.com.au>
2014-08-14 08:10:46 +1000

    Initial commit

---
+++ ws_get_interval_reading_by_customer_key.php
@@ -0,0 +1,36 @@
+<?php
+	/**
+	 * Returns the entire dataset for a given client
+	 */
+
+	# Includes
+	require_once("inc/error.inc.php");
+	require_once("inc/database.inc.php");
+	require_once("inc/security.inc.php");
+	require_once("inc/json.pdo.inc.php");
+
+	# Performs the query and returns XML or JSON
+	try {
+		$p_customer_key = $_REQUEST['customer_key'];
+
+		$sql = <<<ENDSQL
+	select * from interval_reading_mini where customer_key=$p_customer_key
+ENDSQL;
+
+		//echo $sql;
+		$pgconn = pgConnection();
+
+		/*** fetch into an PDOStatement object ***/
+		$recordSet = $pgconn->prepare($sql);
+		$recordSet->execute();
+
+		//header("Content-Type: application/json");
+		// Required to cater for IE
+		header("Content-Type: text/html");
+		echo rs2json($recordSet);
+	}
+	catch (Exception $e) {
+		trigger_error("Caught Exception: " . $e->getMessage(), E_USER_ERROR);
+	}
+
+?>
