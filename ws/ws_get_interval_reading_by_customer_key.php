ad740b44425bbf06bb3f1aea86a6f7d41f05ffb4
Herve Senot <herve.senot@groundtruth.com.au>
2014-10-05 10:55:45 +1100

    Caters for missing data

--- ws/ws_get_interval_reading_by_customer_key.php
+++ ws/ws_get_interval_reading_by_customer_key.php
@@ -14,7 +14,15 @@
 		$p_customer_key = $_REQUEST['customer_key'];
 
 		$sql = <<<ENDSQL
-	select day as d,prd as p,kwh1 as k1,kwh2 as k2,kwh3 as k3, kwh4 as k4 from interval_reading_mini where customer_key=$p_customer_key order by 1,2
+	select all_d_p.d,all_d_p.p,coalesce(t.k1,0.0) k1,coalesce(t.k2,0.0) k2,coalesce(t.k3,0.0) k3,coalesce(t.k4,0.0) k4 from
+	(select * from
+	(select * from generate_series(
+	(select min(day)::integer from interval_reading_mini where customer_key=$p_customer_key),
+	(select max(day)::integer from interval_reading_mini where customer_key=$p_customer_key)
+	) as d) dates, (select * from generate_series(1,48) as p) periods) all_d_p
+	LEFT JOIN
+	(select day as d,prd as p,kwh1 as k1,kwh2 as k2,kwh3 as k3, kwh4 as k4 from interval_reading_mini where customer_key=$p_customer_key) t
+	ON all_d_p.d=t.d and all_d_p.p=t.p ORDER BY 1,2
 ENDSQL;
 
 		//echo $sql;
