4a11189add1d02d0c14a80b7d174f90b56b51b80
Herve Senot <herve.senot@groundtruth.com.au>
2015-01-06 17:58:43 +1100

    Added checkbox to restrict load types

--- ws/ws_customers_by_params.php
+++ ws/ws_customers_by_params.php
@@ -12,6 +12,9 @@
 	# Performs the query and returns XML or JSON
 	try {
 		$p_param1 = $_REQUEST['param1'];
+		$p_param2 = $_REQUEST['param2'];
+		$p_param3 = $_REQUEST['param3'];
+		$p_param4 = $_REQUEST['param4'];
 
 		$sql = <<<ENDSQL
 select customer_key as id from
@@ -19,9 +22,12 @@ trial_customer where customer_key in
 (
 select m.customer_key from interval_reading_mini m
 )
-and customer_key||'' like '%$p_param1%'
+and customer_key||'' like '%$p_param1%' 
+and controlled_load_cnt>=$p_param2 
+and (net_solar_cnt+gross_solar_cnt)>=$p_param3 
+and other_load_cnt>=$p_param4
 order by customer_key
-limit 10
+limit 50
 ENDSQL;
 
 		//echo $sql;
